caa86f9a5e594ac778254295a5541cb140bf8fc9
ZNOUAR <zakaria.nouar2000@gmail.com>
2019-05-13 16:44:09 +0200

    4 Wijzigen student

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,3 @@
-INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
-	(90001,'lo8e-amo1'),
-	(90002,'lo8e-amo2'),
-	(90003,'lo8e-amo3'),
-	(90004,'lo8e-amo4');
+UPDATE studenten
+SET geboortedatum = '2019-01-01'
+WHERE voornaam = 'Osman';
