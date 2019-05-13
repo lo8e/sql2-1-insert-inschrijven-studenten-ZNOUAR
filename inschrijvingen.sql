bdd243d64f50033a59bea97973bf523484218e69
ZNOUAR <zakaria.nouar2000@gmail.com>
2019-05-13 17:10:29 +0200

    6 Overzicht inschrijvingen

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,3 +1,7 @@
-UPDATE studenten
-SET inschrijvings_datum = 'inactief'
-WHERE inschrijvings_datum < '2017-01-01';
+SELECT studenten.ov_nummer,studenten.voornaam,studenten.tussenvoegsel,studenten.achternaam,
+klassen_studenten.klas_code,klassen.cohort,docenten.voorletters,docenten.tussenvoegsel,docenten.achternaam
+FROM studenten
+LEFT JOIN klassen_studenten ON klassen_studenten.klas_code = studenten.ov_nummer
+LEFT JOIN klassen ON klassen.klas_code = klassen_studenten.klas_code
+LEFT JOIN docenten ON docenten.docent_code = klassen.slb_code
+ORDER BY klassen_studenten.klas_code AND studenten.achternaam;
